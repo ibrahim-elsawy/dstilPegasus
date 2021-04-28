abe3e1081ef8ed146ea10e07daca802a587377a1
ibrahim elsawy <53919684+ibrahim-elsawy@users.noreply.github.com>
2021-04-29 00:59:00 +0200

    Update runners.py

--- src/runners/runners.py
+++ src/runners/runners.py
@@ -6,11 +6,8 @@ from catalyst.dl.utils import check_ddp_wrapped
 import torch
 import gc 
 
-class DistilMLMRunner(dl.Runner):
-    """Simplified huggingface Distiller wrapped with catalyst"""
-    
-    
-    def shift_tokens_right(input_ids, pad_token_id):
+
+def shift_tokens_right(input_ids, pad_token_id):
         """Shift input ids one token to the right, and wrap the last non pad token (usually <eos>)."""
         prev_output_tokens = input_ids.clone()
         #print(pad_token_id, input_ids)
@@ -19,7 +16,9 @@ class DistilMLMRunner(dl.Runner):
         prev_output_tokens[:, 0] = input_ids.gather(1, index_of_eos).squeeze()
         prev_output_tokens[:, 1:] = input_ids[:, :-1]
         return prev_output_tokens
-    
+   
+class DistilMLMRunner(dl.Runner):
+    """Simplified huggingface Distiller wrapped with catalyst"""
     
     def _handle_batch(self, batch: Dict[str, torch.Tensor]):
         if check_ddp_wrapped(self.model):
