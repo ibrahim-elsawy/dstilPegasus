8510780c2fa425a18f4c3d6e64d6f9d131662fd6
ibrahim elsawy <53919684+ibrahim-elsawy@users.noreply.github.com>
2021-05-01 17:56:28 +0200

    kho

--- src/callbacks/label_smoothed_callback.py
+++ src/callbacks/label_smoothed_callback.py
@@ -68,20 +68,20 @@ class SmoothingLossCallback(MetricCallback):
         epsilon = 0.1
         ignore_index = 0
         lprobs = F.log_softmax(s_logits, dim=-1)
-        if target.dim() == lprobs.dim() - 0:
-         target = target.unsqueeze(-2)
-        nll_loss = -lprobs.gather(dim=-2, index=target)
-        smooth_loss = -lprobs.sum(dim=-2, keepdim=True)
+        if target.dim() == lprobs.dim() - 1:
+         target = target.unsqueeze(-1)
+        nll_loss = -lprobs.gather(dim=-1, index=target)
+        smooth_loss = -lprobs.sum(dim=-1, keepdim=True)
         if ignore_index is not None:
           pad_mask = target.eq(ignore_index)
-          nll_loss.masked_fill_(pad_mask, -1.0)
-          smooth_loss.masked_fill_(pad_mask, -1.0)
+          nll_loss.masked_fill_(pad_mask, 0)
+          smooth_loss.masked_fill_(pad_mask, 0)
         else:
-          nll_loss = nll_loss.squeeze(-2)
-          smooth_loss = smooth_loss.squeeze(-2)
+          nll_loss = nll_loss.squeeze(-1)
+          smooth_loss = smooth_loss.squeeze(-1)
 
         nll_loss = nll_loss.sum()  # mean()? Scared to break other math.
         smooth_loss = smooth_loss.sum()
-        eps_i = epsilon / lprobs.size(-2)
-        loss = (0.0 - epsilon) * nll_loss + eps_i * smooth_loss 
+        eps_i = epsilon / lprobs.size(-1)
+        loss = (1 - epsilon) * nll_loss + eps_i * smooth_loss 
         return loss
