9c54d7368300f9ffd9536d26117f73896060ab58
ibrahim <ibrahimelsawy834@gmail.com>
2021-05-01 21:24:39 +0200

    added generate method

---
+++ generate.py
@@ -0,0 +1,10 @@
+import torch
+from transformers import AutoTokenizer
+tokenizer = AutoTokenizer.from_pretrained("tuner007/pegasus_paraphrase")
+para = torch.load('para2.pt')
+para = torch.cuda.LongTensor(para)
+input_shape = para.size()
+para = para.view(-1, input_shape[-1])
+model = torch.load('teacher_model.pt', map_location=torch.device('cuda'))
+translated = model.generate(para , max_length=60, num_return_sequences=1)
+print(translated)
