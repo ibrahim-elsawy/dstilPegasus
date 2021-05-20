ad46c87c42b4ded72c1c78b3d335e66f73d2f974
Ahmed Emad <70484736+a7med3mad12@users.noreply.github.com>
2021-05-20 02:15:45 +0200

    Update __init__.py

--- src/callbacks/__init__.py
+++ src/callbacks/__init__.py
@@ -4,7 +4,7 @@ from .masked_language_model_callback import MaskedLanguageModelCallback
 from .mse_loss_callback import MSELossCallback
 from .perplexity_callback import PerplexityMetricCallbackDistillation
 from .label_smoothed_callback import SmoothingLossCallback
-
+from .Cross_ent_loss import CrossentropylossCallback
 __all__ = [
     "CosineLossCallback",
     "MaskedLanguageModelCallback",
