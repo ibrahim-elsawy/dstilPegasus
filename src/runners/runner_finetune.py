915a39370214c89c862f44c4f217b743f8dc999b
ibrahim <ibrahimelsawy834@gmail.com>
2021-05-21 22:39:02 +0200

    bugFixed

--- src/runners/runner_finetune.py
+++ src/runners/runner_finetune.py
@@ -7,15 +7,6 @@ import torch
 import gc 
 
 
-# def shift_tokens_right(input_ids, pad_token_id):
-#         """Shift input ids one token to the right, and wrap the last non pad token (usually <eos>)."""
-#         prev_output_tokens = input_ids.clone()
-#         #print(pad_token_id, input_ids)
-#         x= (input_ids.ne(pad_token_id).sum(dim=1) - 1)
-#         index_of_eos = x.unsqueeze(-1)
-#         prev_output_tokens[:, 0] = input_ids.gather(1, index_of_eos).squeeze()
-#         prev_output_tokens[:, 1:] = input_ids[:, :-1]
-#         return prev_output_tokens
 def shift_tokens_right(input_ids: torch.Tensor, pad_token_id = 0, decoder_start_token_id = 0):
     """
     Shift input ids one token to the right.
@@ -35,7 +26,7 @@ class DistilMLMRunnerFT(dl.Runner):
     
     def _handle_batch(self, batch: Dict[str, torch.Tensor]):
         if check_ddp_wrapped(self.model):
-            teacher, student = (
+             student = (
                 self.model.module["teacher"],
                 self.model.module["student"],
             )
@@ -52,48 +43,24 @@ class DistilMLMRunnerFT(dl.Runner):
         student.to('cuda')
         batch["input_ids"] = batch["input_ids"].to('cuda')
         batch["attention_mask"] = batch["attention_mask"].to('cuda')
-        # decoder_mask = decoder_inputs != 0
-        # decoder_mask = torch.Tensor(batch['decode_mask'])
-        # decoder_ids = torch.Tensor(batch['decode_ids'])
-        # listOfIds = map(lambda x : torch.cuda.LongTensor(x), batch['decode_ids'])
-        # listOfMasks = map(lambda x : torch.cuda.LongTensor(x), batch['decode_mask'])
-        # decoder_mask = torch.stack(list(listOfMasks))
-        # decoder_ids = torch.stack(list(listOfIds))
-#         decoder_ids = torch.zeros(*(batch["input_ids"].shape[0], 60),device='cuda', dtype=torch.long)
         batch['decode_ids'] = shift_tokens_right(batch['decode_ids'], 0)
         batch['decode_ids'] = batch['decode_ids'].to('cuda')        
         studentOutput =student( torch.cuda.LongTensor( batch["input_ids"]), batch["attention_mask"], batch['decode_ids'], output_attentions=True, output_hidden_states=True)
         s_logits = studentOutput.logits
         s_hidden_states = studentOutput.encoder_hidden_states
-        dt_hidden_states = teacherOutput.decoder_hidden_states
         ds_hidden_states = studentOutput.decoder_hidden_states
-#         s_attention_mask = studentOutput.decoder_attentions
         gc.collect()
         torch.cuda.empty_cache()
-        ###################### to use the model outputs
-        #max_length = batch["decode_ids"].shape[-1]
-        #genLabel = teacher.generate(batch["input_ids"], attention_mask=batch["attention_mask"], max_length=max_length, num_beams=1, num_return_sequences=1)
-        #size = genLabel.shape
-        #genLabel = torch.cat((genLabel, torch.cuda.LongTensor(size=(size[0], max_length - size[1])).fill_(0)), dim=1)
-        ###########################
         self.output = OrderedDict()
         self.output["attention_mask"] = shift_tokens_right(batch['decode_mask'], 0)
         self.output["input_attention_mask"] = batch["attention_mask"]
-        #self.output["t_hidden_states"] = t_hidden_states
         self.output["s_hidden_states"] = s_hidden_states
 
 
 
-        #FIXME
-        #self.output["dt_hidden_states"] = dt_hidden_states
         self.output["ds_hidden_states"] = ds_hidden_states
 
 
         self.output["s_logits"] = s_logits
-        #self.output["t_logits"] = t_logits
         self.output["target"] = batch["decode_ids"]
-        #self.output["target"] = genLabel
-        #del genLabel
-        #FIXME
-        #student.to('cpu')
         torch.cuda.empty_cache()
